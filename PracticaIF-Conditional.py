2e8bda11f799e5615552c33f255f0222bbfbadcc
JosueSR <infesnabemji@gmail.com>
2025-03-21 10:47:27 -0600

    03-21-25

---
+++ PracticaIF-Conditional.py
@@ -0,0 +1,45 @@
+"""
+if 5>6: print("Yes")
+else: print("No")
+"""
+'''
+if condition1:
+    statements
+elif condition2:
+    statements
+elif condition3:
+    statements
+else:
+    statements
+'''
+
+'''
+#Preguntar al usuario un numero entre 1 y 10
+
+numero = int(input("Escriba un numero entre 1 y 10"))
+
+if numero < 1 & numero > 10:
+    print(f"Numero no valido")
+elif numero >= 8:
+    print(f"Numero mayor que 8, numero en rango alto")
+elif numero >=5:
+    print(f"Numero entre 5 y 8, numero en rango medio")
+else:
+    print(f"Numero entre 1 y 5, numero rango bajo") 
+'''
+'''
+#Operadores en Python
+# ==, !=, >, <, >=, <= -------------Comparisson
+# AND OR NOT ------------------------Logicos
+# is, is not ------------------------Identidad - 
+# in, not in ------------------------- Pertenencia 
+'''
+
+
+
+
+
+
+
+
+
